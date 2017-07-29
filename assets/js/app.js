293736819af6e1fa651669e4b1ef4239358d8e22
Tim Zwolak <timothypage@gmail.com>
2017-07-29 14:57:50 -0600

    purge all traces of react

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,16 +12,10 @@
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
 import "phoenix_html"
-import "react-phoenix"
+
 // Import local files
 //
 // Local files can be imported directly using relative
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
-
-import Hello from "./components/hello"
-
-window.ReactPhoenixExample = {
-  Hello
-}
